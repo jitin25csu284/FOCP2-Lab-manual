cf77649d559ca40744a47f791ed6143dde5632ea
jitin25csu284 <jitin25csu284@ncuindia.edu>
2026-05-08 22:48:14 +0530

    Create q25.cpp

---
+++ q25.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class PrimeRange{
+public:
+    void display(int a,int b){
+
+        for(int i=a;i<=b;i++){
+            int flag=0;
+
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    flag=1;
+                    break;
+                }
+            }
+
+            if(flag==0 && i>1)
+                cout<<i<<" ";
+        }
+    }
+};
+
+int main(){
+    PrimeRange p;
+    int a,b;
+
+    cout<<"Enter limits: ";
+    cin>>a>>b;
+
+    p.display(a,b);
+}
