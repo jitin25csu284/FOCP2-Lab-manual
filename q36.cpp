aeffcd95474baf1c84632156582878503ab47f6e
jitin25csu284 <jitin25csu284@ncuindia.edu>
2026-05-08 22:52:24 +0530

    Create q36.cpp

---
+++ q36.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class MatrixMul{
+public:
+    void multiply(){
+        int a[2][2],b[2][2],c[2][2];
+
+        cout<<"Enter matrix A:\n";
+        for(int i=0;i<2;i++)
+            for(int j=0;j<2;j++)
+                cin>>a[i][j];
+
+        cout<<"Enter matrix B:\n";
+        for(int i=0;i<2;i++)
+            for(int j=0;j<2;j++)
+                cin>>b[i][j];
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                c[i][j]=0;
+                for(int k=0;k<2;k++)
+                    c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+
+        cout<<"Result:\n";
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++)
+                cout<<c[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    MatrixMul m;
+    m.multiply();
+}
