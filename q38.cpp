91f01de1722c78a7ee813e0d2ede2139e677fed7
jitin25csu284 <jitin25csu284@ncuindia.edu>
2026-05-08 22:53:00 +0530

    Create q38.cpp

---
+++ q38.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Pascal{
+public:
+    void display(int n){
+        for(int i=0;i<n;i++){
+            int num=1;
+
+            for(int j=0;j<=i;j++){
+                cout<<num<<" ";
+                num=num*(i-j)/(j+1);
+            }
+
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    Pascal p;
+    int n;
+
+    cout<<"Enter rows: ";
+    cin>>n;
+
+    p.display(n);
+}
